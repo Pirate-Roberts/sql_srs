dbf371e40362c1e4c5b54f626c15dc57b581a778
Jonathan TOURAILLE <jonathan.touraille@vinci-construction.com>
2024-07-14 22:07:59 +0200

    refactor check user solution and get exercise to 2 functions

--- app.py
+++ app.py
@@ -20,6 +20,53 @@ if "exercises_sql_tables.duckdb" not in os.listdir("data"):
 # Connection à la database
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
+
+def check_users_solution(user_query: str) -> None:
+    """
+    Check that user SQL query is correct by :
+    1 : Checking the columns
+    2 : Checking the values
+    :param user_query: a string containing the query inserted by the user
+    """
+    result = con.execute(user_query).df()
+    st.dataframe(result)
+    if len(result.columns) != len(solution_df.columns):
+        st.write("Some columns are missing")
+    try:
+        # On utilise le même agencement de colonnes
+        result = result[solution_df.columns]
+    except KeyError:
+        st.write("Some columns are missing")
+    nb_lines_difference = result.shape[0] - solution_df.shape[0]
+    if nb_lines_difference != 0:
+        st.write(
+            f"Result has a {nb_lines_difference} lines difference with the solution"
+        )
+    try:
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        print(f"Erreur : {e}")
+    except ValueError as e:
+        print(f"Erreur : {e}")
+
+
+def get_exercises(exercise_theme):
+    """
+    Get exercises from the chosen theme
+    :param exercise_theme : string containing the chose theme for the exercises
+    :return df: a dataframe containing all the exercises associated to the chosen theme
+    """
+    if exercise_theme:
+        st.write(f"You selected: {exercise_theme}")
+        select_exercise_query = f"SELECT * FROM memory_state WHERE theme = '{exercise_theme}' ORDER BY last_reviewed"
+    else:
+        select_exercise_query = "SELECT * FROM memory_state ORDER BY last_reviewed"
+    df = con.execute(select_exercise_query).df()
+    st.write(df)
+
+    return df
+
+
 # Menu déroulant dans la sidebar
 with st.sidebar:
     theme_list = (
@@ -34,14 +81,7 @@ with st.sidebar:
         placeholder="Select theme...",
     )
 
-    if theme:
-        st.write(f"You selected: {theme}")
-        select_exercise_query = f"SELECT * FROM memory_state WHERE theme = '{theme}' ORDER BY last_reviewed"
-    else:
-        select_exercise_query = "SELECT * FROM memory_state ORDER BY last_reviewed"
-
-    exercise_df = con.execute(select_exercise_query).df()
-    st.write(exercise_df)
+    exercise_df = get_exercises(theme)
 
     solution_file = exercise_df.loc[0, "solution"]
     with open(os.path.join("solutions", solution_file), "r") as f:
@@ -52,30 +92,7 @@ st.header("Entrez votre requète SQL:")
 query = st.text_area(label="Votre code SQL ici", key="user_input")
 
 if query:
-    result = con.execute(query).df()
-    st.dataframe(result)
-
-    if len(result.columns) != len(solution_df.columns):
-        st.write("Some columns are missing")
-
-    try:
-        # On utilise le même agencement de colonnes
-        result = result[solution_df.columns]
-    except KeyError as e:
-        st.write("Some columns are missing")
-
-    nb_lines_difference = result.shape[0] - solution_df.shape[0]
-
-    if nb_lines_difference != 0:
-        st.write(
-            f"Result has a {nb_lines_difference} lines difference with the solution"
-        )
-    try:
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        print(f"Erreur : {e}")
-    except ValueError as e:
-        print(f"Erreur : {e}")
+    check_users_solution(query)
 
 tab1, tab2 = st.tabs(["Tables", "Solution"])
 
